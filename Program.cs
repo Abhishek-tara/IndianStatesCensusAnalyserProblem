48b30bb0a4d13e6a157e6205c97e3b8a7d4fd719
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-12 11:28:06 +0530

    Added Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStatesCensusAnalyserProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indian State Census Analyser Problem");
+        }
+    }
+}
