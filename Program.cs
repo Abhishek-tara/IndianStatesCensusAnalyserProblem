3498adee4645f7c709bd6cb2b40826f0dbca62c6
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-16 14:34:55 +0530

    Added Ability to load Indian Census state data from csv file

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Indian State Census Analyser Problem");
+
+            string filePath = "D:\\c#\\257\\IndianStatesCensusAnalyserProblem\\IndianStatesCensusAnalyserProblem\\StaeCensus.csv";
+            StateCensusAnalyser analyser = new StateCensusAnalyser(filePath);
+            int numberOfRecords = analyser.LoadDataFromCSV();
+            Console.WriteLine("Number of records in the file: " + numberOfRecords);
         }
     }
 }
